58e6ebc193f44e8dd95acd3d5a36aa198b097f63
EmilyT <emilython@hotmail.com>
2021-10-25 20:20:39 -0500

    added extra code

--- server.js
+++ server.js
@@ -53,3 +53,14 @@ app.get('/api/animals', (req, res) => {
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
+
+  if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
